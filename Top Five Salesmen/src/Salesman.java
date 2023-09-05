892766ee8d23c51d99b18893cf65b2d3c406c3b0
Silaswalter <129095844+Silaswalter@users.noreply.github.com>
2023-09-05 12:15:15 -0500

    added function to topFiveSalesMen

--- Top Five Salesmen/src/Salesman.java
+++ Top Five Salesmen/src/Salesman.java
@@ -7,5 +7,30 @@ public class Salesman {
 		this.name = name;
 		this.sales = sales;
 	}
-	
+	/*for(int j = 0; j < topFive.length; j++ )
+	{
+		for(int i = 0; i < dudes.length; i++) 
+		{
+			if(dudes[i].inTopFive == false)
+			{
+				if(first < dudes[i].sales) 
+				{
+					first = dudes[i].sales;
+					topFive[j] = dudes[i];
+				}
+			}	
+		}
+		for(int k = 0; k < dudes.length; k++)
+		{
+			if(first == dudes[k].sales)
+			{
+				dudes[k].inTopFive = true;
+				first = 0;
+			}
+			
+		}
+		
+		System.out.println(topFive[j].name);
+		
+	}*/
 }
